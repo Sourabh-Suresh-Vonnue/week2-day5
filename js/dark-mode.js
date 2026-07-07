1b013bb15f7b322771defa189e82ed2bc0ea6e68
Sourabh Suresh <sourabh.suresh@vonnue@gmail.com>
2026-07-07 11:20:20 +0530

    refactor: dark mode data-theme changed from body to html tag

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,17 +1,17 @@
 const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme == 'dark') {
-  document.body.setAttribute('data-theme', 'dark');
+  document.documentElement.setAttribute('data-theme', 'dark');
   document.getElementById('dark-mode-btn').setAttribute('aria-pressed', true);
 } else {
-  document.body.setAttribute('data-theme', 'light');
+  document.documentElement.setAttribute('data-theme', 'light');
   document.getElementById('dark-mode-btn').setAttribute('aria-pressed', false);
 }
 
 function darkMode() {
   let newTheme = 'dark';
   let newText = 'Light Mode';
-  const currTheme = document.body.getAttribute('data-theme');
+  const currTheme = document.documentElement.getAttribute('data-theme');
   let toggleBtn = document.getElementById('dark-mode-btn');
   let ariaPressed = true;
 
@@ -20,7 +20,7 @@ function darkMode() {
     newText = 'Dark Mode';
     ariaPressed = false;
   }
-  document.body.setAttribute('data-theme', newTheme);
+  document.documentElement.setAttribute('data-theme', newTheme);
   localStorage.setItem('theme', newTheme);
   toggleBtn.innerText = newText;
   toggleBtn.setAttribute('aria-pressed', ariaPressed);
