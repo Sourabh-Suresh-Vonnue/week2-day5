0d44337d1157a7d8bd0ae1014f5959ef4b9e3431
Sourabh Suresh <sourabh.suresh@vonnue@gmail.com>
2026-07-08 16:22:54 +0530

    refactor: week4 day1 task4 - darkMode, accordion and nav imported into main.js

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,6 +1,7 @@
-const nav = document.getElementById('main-nav');
-const navUl = document.createElement('ul');
-navUl.innerHTML = `<li><a href="index.html">Home</a></li>
+export function init() {
+  const nav = document.getElementById('main-nav');
+  const navUl = document.createElement('ul');
+  navUl.innerHTML = `<li><a href="index.html">Home</a></li>
           <li><a href="about.html">About</a></li>
           <li><a href="services.html">Services</a></li>
           <li><a href="team.html">Team</a></li>
@@ -8,16 +9,17 @@ navUl.innerHTML = `<li><a href="index.html">Home</a></li>
           <li><a href="gallery.html">Gallery</a></li>
           <li><a href="blog.html">Blog</a></li>`;
 
-if (window.location.pathname == '/') {
-  const homePageLink = navUl.querySelector('a[href="index.html"');
-  homePageLink.setAttribute('aria-current', 'page');
-} else {
-  const url = window.location.pathname;
-  const links = Array.from(navUl.getElementsByTagName('a'));
-  const link = links.find((link) => link.pathname == url);
-  if (link) {
-    link.setAttribute('aria-current', 'page');
+  if (window.location.pathname == '/') {
+    const homePageLink = navUl.querySelector('a[href="index.html"');
+    homePageLink.setAttribute('aria-current', 'page');
+  } else {
+    const url = window.location.pathname;
+    const links = Array.from(navUl.getElementsByTagName('a'));
+    const link = links.find((link) => link.pathname == url);
+    if (link) {
+      link.setAttribute('aria-current', 'page');
+    }
   }
-}
 
-nav.appendChild(navUl);
+  nav.appendChild(navUl);
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { init as darkModeInit } from './components/darkMode.js';
+import { init as accordionInit } from './components/accordion.js';
+import { init as navInit } from './components/nav.js';
+
+darkModeInit();
+accordionInit();
+navInit();
